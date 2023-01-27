7896ca6e0baf4af0aeb16106325cfd5c7704af5a
kalboussik <63822767+kalboussik@users.noreply.github.com>
2023-01-27 01:51:08 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -2,4 +2,3 @@ start_delay=10
 stop_delay=10
 #cam = "rtsp://admin:ZGLHBD@81.218.214.248:554/Streaming/Channels/101"
 cam = "rtsp://admin:ZGLHBD@81.218.214.248:554/Streaming/Channels/102"
-channel = "rtmp://a.rtmp.youtube.com/live2/pwcx-tbkz-dqyv-f404-7xpe"
