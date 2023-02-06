053fc71558b0d8c017ef787d6132a6cdcbb5babe
kalboussik <63822767+kalboussik@users.noreply.github.com>
2023-02-06 13:20:42 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 start_delay=10
 stop_delay=10
-#cam = "rtsp://admin:ZGLHBD@81.218.214.248:554/Streaming/Channels/101"
-cam = "rtsp://admin:ZGLHBD@81.218.214.248:554/Streaming/Channels/102"
+#cam = ""
+cam = ""
